dc625aa9ac557155a0884222f4397e1372911b5e
devsia93 <sia93r@gmail.com>
2020-11-12 23:34:46 +0300

    Initial commit

---
+++ samplesite/bboard/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+from .models import Bb, Rubric
+
+
+def index(request):
+    bbs = Bb.objects.all()
+    rubrics = Rubric.objects.all()
+    context = {"bbs": bbs, "rubrics": rubrics}
+    # template = loader.get_template('bboard/index.html')
+    # return HttpResponse(template.render(context=context, request=request))
+    return render(request, 'bboard/index.html', context)
+
+
+def by_rubric(request, rubric_id):
+    bbs = Bb.objects.filter(rubric=rubric_id)
+    rubrics = Rubric.objects.all()
+    current_rubric = Rubric.objects.get(pk=rubric_id)
+    context = {'bbs': bbs, 'rubrics': rubrics, 'current_rubric': current_rubric}
+    return render(request, 'bboard/by_rubric.html', context)
